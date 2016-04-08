76494f857eba2aa992368ccc0291ab50e0befee3
sdaniels <daniels.scott.r@gmail.com>
2016-04-08 19:57:11 -0400

    initial project setup

---
+++ app/index.php
@@ -0,0 +1,21 @@
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/_/inc/init.php'); ?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js ie ie6 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js ie ie7 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js ie ie8 lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js ie ie9 lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <title>Weather Forecast</title>
+    <meta name="description" content="">
+    <?php require("_/inc/head.php"); ?>
+</head>
+<body>
+    
+
+    <!-- JAVASCRIPT -->
+    <?php require('_/inc/analytics.php'); ?>
+
+    <?php require('_/inc/tail.php'); ?>
+</body>
+</html>
